eb58d285ade2928263f0e9e5788f119ce0c37e66
Eduard Gataullin <edikgat@gmail.com>
2020-07-05 21:28:52 +0300

    cleanup & remove not using files

--- app/services/yml_config_reader.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module YMLConfigReader
-  def self.fetch(filepath, env_name)
-    complete = YAML.load(ERB.new(File.read(filepath)).result)
-    current = complete[env_name] || complete['default']
-    current.symbolize_keys
-  end
-end
